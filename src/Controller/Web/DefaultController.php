4dd88b5868229b1ed97b9bd34389147c70c16e7a
aihe <torghay@howzhi.com>
2017-12-01 11:50:14 +0600

    添加调试工具

---
+++ src/Controller/Web/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller\Web;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+ public function index()
+ {
+     return $this->render('base.html.twig');
+ }
+}
