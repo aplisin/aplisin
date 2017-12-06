4d25764ec1832666b5ef684405afba4feaa2f8d9
aihe <torghay@howzhi.com>
2017-12-06 08:28:51 +0600

    设置用户权限

--- src/Controller/Admin/DefaultController.php
+++ src/Controller/Admin/DefaultController.php
@@ -5,11 +5,16 @@ namespace App\Controller\Admin;
 use App\Controller\BaseController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
+
+/**
+ * Class DefaultController
+ * @package App\Controller\Admin
+ * @Security("is_granted('ROLE_ADMIN')")
+ */
 class DefaultController extends BaseController
 {
     public function index()
     {
-        $this->denyAccessUnlessGranted('ROLE_ADMIN');
         return $this->render('admin/default/index.html.twig');
     }
 }
