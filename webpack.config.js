68788a6384bc482afa1e957ad269040550395ed6
torghay <torghay@yeah.net>
2018-02-27 20:38:54 +0800

    添加前端资源

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,58 @@
 var Encore = require('@symfony/webpack-encore');
 
 Encore
-    // the project directory where compiled assets will be stored
-    .setOutputPath('public/build/')
+// the project directory where compiled assets will be stored
+    .setOutputPath('public/assets/')
     // the public path used by the web server to access the previous directory
-    .setPublicPath('/build')
-    .cleanupOutputBeforeBuild()
-    .enableSourceMaps(!Encore.isProduction())
-    // uncomment to create hashed filenames (e.g. app.abc123.css)
-    // .enableVersioning(Encore.isProduction())
+    .setPublicPath('/assets')
 
     // uncomment to define the assets of the project
-    // .addEntry('js/app', './assets/js/app.js')
-    // .addStyleEntry('css/app', './assets/css/app.scss')
+    // .addEntry('web/app', './assets/web/app.js')
+    .addStyleEntry('web/app', './assets/web/app.scss')
+    .addStyleEntry('admin/app', './assets/admin/app.scss')
+
+    .addEntry('libs/fix-ie', [
+        'html5shiv',
+        'es5-shim',
+        'es5-shim/es5-sham'
+    ])
+
+    .createSharedEntry('libs/base', [
+        'jquery',
+        'codeages-design',
+        'codeages-design/src/less/codeages-design.less',
+
+        // you can also extract CSS - this will create a 'vendor.css' file
+        // this CSS will *not* be included in page1.css or page2.css anymore
+        './assets/libs/base.scss'
+    ])
+
+    // uncomment if you use lessfiles
+    .enableLessLoader()
 
     // uncomment if you use Sass/SCSS files
-    // .enableSassLoader()
+    .enableSassLoader()
 
     // uncomment for legacy applications that require $/jQuery as a global variable
-    // .autoProvidejQuery()
+    .autoProvidejQuery()
+
+    // you can use this method to provide other common global variables,
+    // such as '_' for the 'underscore' library
+    .autoProvideVariables({
+        'window.jQuery': 'jquery',
+        'window.Tether': 'tether'
+    })
+
+    .enableSourceMaps(!Encore.isProduction())
+
+    // uncomment to create hashed filenames (e.g. app.abc123.css)
+    .enableVersioning(Encore.isProduction())
+
+    // empty the outputPath dir before each build
+    .cleanupOutputBeforeBuild()
+
+    // show OS notifications when builds finish/fail
+    .enableBuildNotifications()
 ;
 
 module.exports = Encore.getWebpackConfig();
