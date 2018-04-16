139921ce55a26c5610bd1488933434fd39df13b4
torghay <torghay@yeah.net>
2018-04-16 11:38:27 +0800

    jwt guard

---
+++ src/Security/Guard/JwtTokenAuthenticator.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Security\Guard;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Security\Guard\JWTTokenAuthenticator as BaseAuthenticator;
+use Lexik\Bundle\JWTAuthenticationBundle\TokenExtractor\AuthorizationHeaderTokenExtractor;
+use Lexik\Bundle\JWTAuthenticationBundle\TokenExtractor\CookieTokenExtractor;
+use Lexik\Bundle\JWTAuthenticationBundle\TokenExtractor\QueryParameterTokenExtractor;
+use Lexik\Bundle\JWTAuthenticationBundle\TokenExtractor\TokenExtractorInterface;
+
+class JwtTokenAuthenticator extends BaseAuthenticator
+{
+    /**
+     * @return AuthorizationHeaderTokenExtractor|TokenExtractorInterface
+     */
+    protected function getTokenExtractor()
+    {
+        // Return a custom extractor, no matter of what are configured
+        return new AuthorizationHeaderTokenExtractor('', 'Authorization');
+        // Or retrieve the chain token extractor for mapping/unmapping extractors for this authenticator
+        $chainExtractor =  parent::getTokenExtractor();
+        $chainExtractor->clearMap();
+        // Or only remove a specific extractor
+        $chainTokenExtractor->removeExtractor(function (TokenExtractorInterface $extractor) {
+            return $extractor instanceof CookieTokenExtractor;
+        });
+        // Add a new query parameter extractor to the configured ones
+        $chainExtractor->addExtractor(new QueryParameterTokenExtractor('jwt'));
+        // Return the chain token extractor with the new map
+        return $chainTokenExtractor;
+    }
+}
